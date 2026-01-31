8df65104c113b80bc8017b97e2e9302e8ff2648b
tasuku43 <43329036+tasuku43@users.noreply.github.com>
2026-01-31 12:54:17 +0000

    Update Homebrew formula for v0.2.6

--- Formula/gion.rb
+++ Formula/gion.rb
@@ -3,25 +3,25 @@ class Gion < Formula
   homepage "https://github.com/tasuku43/gion"
   license "MIT"
 
-  version "0.2.5"
+  version "0.2.6"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/gion/releases/download/v0.2.5/gion_v0.2.5_macos_arm64.tar.gz"
-      sha256 "ad091ff83aef75d367712555319d1cacfcaf1803342aad612fe4a014ab8461bd"
+      url "https://github.com/tasuku43/gion/releases/download/v0.2.6/gion_v0.2.6_macos_arm64.tar.gz"
+      sha256 "776c74246de17532df4a4ec8ffdbf80e626a5989b8c4f5455d70c79bf46918d8"
     else
-      url "https://github.com/tasuku43/gion/releases/download/v0.2.5/gion_v0.2.5_macos_x64.tar.gz"
-      sha256 "c2f9df89297ca38acac4c92e174968f816cc72f6bd14ac3b0ea48291ade331b7"
+      url "https://github.com/tasuku43/gion/releases/download/v0.2.6/gion_v0.2.6_macos_x64.tar.gz"
+      sha256 "c0f67a0dd6888ac09f9eadacc65370a4b22c8f1a22883ea4f4455f7af581d030"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/gion/releases/download/v0.2.5/gion_v0.2.5_linux_arm64.tar.gz"
-      sha256 "50f7a725214b658718d11d29a484cff182940377415d75969d200775deb66340"
+      url "https://github.com/tasuku43/gion/releases/download/v0.2.6/gion_v0.2.6_linux_arm64.tar.gz"
+      sha256 "4620067e5d17611186f2edf16983f3b7bc2d0202424875227b2271e61f2088ae"
     else
-      url "https://github.com/tasuku43/gion/releases/download/v0.2.5/gion_v0.2.5_linux_x64.tar.gz"
-      sha256 "ee1fd13a98ba3839daa586fd5282337d1855cfe10563e4ebbd0626e69f2ed698"
+      url "https://github.com/tasuku43/gion/releases/download/v0.2.6/gion_v0.2.6_linux_x64.tar.gz"
+      sha256 "a5b9a30ebe0637cc608b52bcfd39a0041d4be5e3e2cc2a99ba2333bac25da672"
     end
   end
 
